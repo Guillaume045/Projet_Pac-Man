9720129972a50c67e4821f7f26299dbf61dbb91c
lguillau <guillaume.lanfroid@ynoc.com>
2024-01-22 15:28:32 +0100

    resoudre les bug

--- Pac-Man/Assets/Script/Passage.cs
+++ Pac-Man/Assets/Script/Passage.cs
@@ -1,22 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Passage : MonoBehaviour
 {
     public Transform connection;
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Vector3 possition = other.transform.position;
         possition.x = this.connection.position.x;
