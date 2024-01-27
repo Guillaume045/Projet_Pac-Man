8e55348a4b4fe515d81ac76edca6dc4609c7ca5f
lguillau <guillaume.lanfroid@ynoc.com>
2024-01-27 15:38:58 +0100

    apparence

--- Pac-Man/Assets/Script/Game_jouer/Fantôme.cs
+++ Pac-Man/Assets/Script/Game_jouer/Fantôme.cs
@@ -45,7 +45,6 @@ public class Fantôme : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("joueur touché");
         if (collision.collider.CompareTag("Player"))
         {
             Respawn();
